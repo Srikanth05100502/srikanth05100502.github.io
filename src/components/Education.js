105ee81f857125b96f7204286551cf7024af555b
srikanthbollikonda <srikanthbollikonda0510@gmail.com>
2025-06-19 11:18:39 +0530

    Create Education.js

---
+++ src/components/Education.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Education() {
+  return (
+    <section>
+      <h2>Education</h2>
+      <ul>
+        <li><strong>GVP College of Engineering</strong>, Bachelor of Technology in Computer Science and Engineering (2018-2021)</li>
+        <li><strong>AANM & VVRSR Polytechnic</strong>, Diploma in Computer Science and Engineering (2015-2017)</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Education;
