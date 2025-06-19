dd6c089d36245c3e841d05d730c2e7c6790b5107
srikanthbollikonda <srikanthbollikonda0510@gmail.com>
2025-06-19 11:17:13 +0530

    Create Summary.js

---
+++ src/components/Summary.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Summary() {
+  return (
+    <section>
+      <h2>Summary</h2>
+      <p>
+        Results-driven data engineer with extensive experience in developing scalable ETL pipelines on GCP using Cloud Data Fusion, BigQuery, and Python, achieving significant cost reductions and improved data processing times. Proficient in designing data warehousing solutions, optimizing data models, and implementing data governance practices to ensure high data integrity and compliance. Skilled in collaborating with cross-functional teams to deliver tailored data solutions and automate workflows using GCP Airflow. Strong background in shell scripting and SQL, with a focus on enhancing operational efficiency and data quality.
+      </p>
+    </section>
+  );
+}
+
+export default Summary;
