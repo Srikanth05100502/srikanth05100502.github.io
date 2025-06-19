d1fa57fd9896e656e96c544c8ddb4dc8e0a1037d
srikanthbollikonda <srikanthbollikonda0510@gmail.com>
2025-06-19 11:17:40 +0530

    Create Experience.js

---
+++ src/components/Experience.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Experience() {
+  return (
+    <section>
+      <h2>Experience</h2>
+      <h3>Digital Specialist Engineer, Infosys Ltd <span style={{fontWeight: 400}}>(Jan 2022 – Present)</span></h3>
+      <ul>
+        <li>Developed scalable, efficient, and reliable ETL pipelines on GCP using Cloud Data Fusion, BigQuery, Cloud Storage, and Python, achieving a 30% reduction in operational costs and faster data processing.</li>
+        <li>Designed and implemented Data warehousing solutions leveraging BigQuery, Airflow, Cloud Storage, Dataproc, and Python to meet business analytical requirements, ensuring 99.9% data accuracy.</li>
+        <li>Led the migration of on-premises Hadoop jobs to GCP using cloud native tools such as Big Query, Airflow, Composer, Cloud Storage, resulting in a 20% reduction in data processing time and significant cost savings.</li>
+        <li>Integrated data from diverse sources, including structured and semi-structured into GCP environments.</li>
+        <li>Established data quality checks and data governance best practices to uphold high data integrity.</li>
+        <li>Continuously monitored and optimized the performance of data pipelines and queries for cost-effectiveness and efficiency.</li>
+        <li>Collaborated closely with data scientists, analysts, and other stakeholders to understand data requirements and deliver tailored data solutions that align with business objectives.</li>
+        <li>Developed Python scripts for data processing, automation, and seamless integration with GCP services.</li>
+        <li>Implemented data governance practices encompassing data security, privacy, and adherence to industry standards.</li>
+        <li>Created and maintained efficient data models in BigQuery to support analytics and reporting needs.</li>
+        <li>Scheduled and automated ETL workflows using GCP Airflow to enhance operational efficiency.</li>
+        <li>Utilized Git for version control to manage and track changes in source code.</li>
+        <li>Developed complex SQL queries using Big Query to transform data, leading to a 15% improvement in data accuracy.</li>
+        <li>Developed and automated control report generation using GCP Airflow and Python, delivering insights and sending timely email notifications to stakeholders.</li>
+        <li>Designed and implemented ETL processes using HQL and Hive tables to efficiently extract, transform, and load large datasets.</li>
+        <li>Developed and maintained shell scripts to automate system tasks and streamline workflows.</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Experience;
