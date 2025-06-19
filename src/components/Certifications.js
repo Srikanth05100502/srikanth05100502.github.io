289e1362b4345b8301fbbce4019e4ef1581b85bf
srikanthbollikonda <srikanthbollikonda0510@gmail.com>
2025-06-19 11:19:01 +0530

    Create Certifications.js

---
+++ src/components/Certifications.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Certifications() {
+  return (
+    <section>
+      <h2>Certifications</h2>
+      <ul>
+        <li>Google Cloud Certified Professional Data Engineer (GCP PDE)</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Certifications;
