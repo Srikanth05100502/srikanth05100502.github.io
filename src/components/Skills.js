31bad5fb8a90331ca29741cd87c531676bb6dc33
srikanthbollikonda <srikanthbollikonda0510@gmail.com>
2025-06-19 11:18:09 +0530

    Create Skills.js

---
+++ src/components/Skills.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <section>
+      <h2>Technical Skills</h2>
+      <ul>
+        <li><strong>Cloud Platforms:</strong> Google Cloud Platform (GCP)</li>
+        <li><strong>GCP Cloud Services:</strong> Cloud Composer, Cloud Storage, Airflow, BigQuery, Data Fusion, Dataproc</li>
+        <li><strong>Query Languages:</strong> SQL, Hive Query Language (HQL)</li>
+        <li><strong>Programming:</strong> Python, Shell Scripting</li>
+        <li><strong>Tools:</strong> Git, Hive</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
